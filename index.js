0de8b29323bb8a0db97938797eae1462d5db1ad6
sajjadhossain <sajjadhossain0756@gmail.com>
2024-09-02 09:47:17 +0600

    task-2 complete

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 // Js-day-4 about loop ::
 // task-1 stat here;
 
-for(var i = 0; i <= 200; i++){
+// for(let i = 0; i <= 200; i++){
 
-   console.log(i);
+//    console.log(i);
 
-   if(i == 100){
+//    if(i == 100){
 
-      break;  
-   }  
+//       break;  
+//    }  
+// }
+
+// task-2 start here;
+let num = 1;
+let sum = 0;
+
+while(num <= 20){
+    
+    sum += num;
+    if(sum >= 100){
+        break;
+    }
+    num++
 }
+console.log(sum);
