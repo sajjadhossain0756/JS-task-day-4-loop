1db67f7b7be64f3e767bb6515d0be31e3970e08c
sajjadhossain <sajjadhossain0756@gmail.com>
2024-09-03 09:48:32 +0600

    task-5 complete

--- index.js
+++ index.js
@@ -25,21 +25,29 @@ while(num <= 20){
 }
 console.log(sum);
 
-// task-3 start here;
+// // task-3 start here;
 
-for (let i = 1; i <= 100; i++) {
-    if (Number.isInteger(Math.sqrt(i))) { // Check if the square root of the number is an integer
-        console.log(`Encountered the first square number: ${i}`);
-        break;
-    }
-}
+// for (let i = 1; i <= 100; i++) {
+//     if (Number.isInteger(Math.sqrt(i))) { // Check if the square root of the number is an integer
+//         console.log(`Encountered the first square number: ${i}`);
+//         break;
+//     }
+// }
 
-// task-4 start here;
-for(let i = 1; i <= 40; i++){
-    if(i % 2 !== 0){
-        continue;  
-    }
-    console.log(i);
+// // task-4 start here;
+// for(let i = 1; i <= 40; i++){
+//     if(i % 2 !== 0){
+//         continue;  
+//     }
+//     console.log(i);
     
+// }
+
+// task-5 start here;
+for(let x = 55; x <= 85; x++){
+    if(x % 5 == 0){
+        continue;
+    }
+    console.log(x);
 }
 
